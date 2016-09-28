e04d58d390b1f342de03774cd6620d838a3b47ea
David Lara <davidlarachirinos@gmail.com>
2016-09-27 23:07:54 -0400

    Update conn.php

--- sistema/controlador/basedatos/conn.php
+++ sistema/controlador/basedatos/conn.php
@@ -1,19 +1,10 @@
 <?php
 
-	$user = 'david'; // usuario de la db
-	$password = '6146568'; //contraseña
+	$user = ''; // usuario de la db
+	$password = ''; //contraseña
 	$dsn = 'Driver={SQL Server Native Client 11.0};Server=localhost;Database=sisact';
-
-
-$odbc = odbc_connect(	
-
-					$dsn, 
-					$user, 
-					$password
-					
-					);
-if ($odbc) {
-	echo "";
-}else {
-	echo "Hubo un error";
-}
+	$odbc = odbc_connect(	
+		$dsn, 
+		$user, 
+		$password
+		);
